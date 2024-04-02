919f6665b741795e96493b16e5d3dbf762996e5b
FabiolaLutrario <fabiolalutrario@gmail.com>
2024-04-02 11:04:53 -0300

    Changing db config

--- api/config/db.config.ts
+++ api/config/db.config.ts
@@ -2,19 +2,19 @@ import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 dotenv.config();
 
-/* const db = new Sequelize(
+ const db = new Sequelize(
   process.env.POSTGRES_DB_NAME ?? "appbox",
   process.env.POSTGRES_DB_USER ?? "postgres",
-  process.env.POSTGRES_DB_PASSWORD ?? "appbox",
+  process.env.POSTGRES_DB_PASSWORD ?? "appboxdelivery",
   {
     host: "postgres-db",
     //port: process.env.POSTGRES_DB_DOCKER_PORT,
     dialect: "postgres",
     logging: false,
   }
-); */
+); 
 
- const db = new Sequelize(
+/*  const db = new Sequelize(
   process.env.POSTGRES_DB_NAME ?? "appbox",
   process.env.POSTGRES_DB_USER ?? "postgres",
   process.env.POSTGRES_DB_PASSWORD ?? "appbox",
@@ -30,7 +30,7 @@ dotenv.config();
     },
     logging: false,
   }
-);  
+);   */
 
 /* const db = new Sequelize(
   "postgresql://uyw3zp4yythtmky2itkk:UzUqOZsvKFxjBKeRTgCn9x5JEYVC8v@bjuqfle74brsz7bhh74l-postgresql.services.clever-cloud.com:50013/bjuqfle74brsz7bhh74l",
