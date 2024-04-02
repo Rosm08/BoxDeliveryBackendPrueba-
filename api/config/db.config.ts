a86ac74020b4f806e016ad9a1e8db73e7c0a33a1
FabiolaLutrario <fabiolalutrario@gmail.com>
2024-04-01 21:29:00 -0300

    Updating pipeline.yml and db.config.ts

--- api/config/db.config.ts
+++ api/config/db.config.ts
@@ -2,7 +2,7 @@ import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 dotenv.config();
 
-const db = new Sequelize(
+/* const db = new Sequelize(
   process.env.POSTGRES_DB_NAME ?? "appbox",
   process.env.POSTGRES_DB_USER ?? "postgres",
   process.env.POSTGRES_DB_PASSWORD ?? "appbox",
@@ -12,7 +12,15 @@ const db = new Sequelize(
     dialect: "postgres",
     logging: false,
   }
-);
+); */
+
+ const db = new Sequelize(
+  "appbox.c9o888e04yjc.us-east-2.rds.amazonaws.com",
+  {
+    dialect: "postgres",
+    logging: false,
+  }
+); 
 
 /* const db = new Sequelize(
   "postgresql://uyw3zp4yythtmky2itkk:UzUqOZsvKFxjBKeRTgCn9x5JEYVC8v@bjuqfle74brsz7bhh74l-postgresql.services.clever-cloud.com:50013/bjuqfle74brsz7bhh74l",
