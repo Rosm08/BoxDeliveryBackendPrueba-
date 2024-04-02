fcdd27491908f58f304285dd0b1bbce999de4e19
FabiolaLutrario <fabiolalutrario@gmail.com>
2024-04-02 00:44:47 -0300

    Changing cors origin into server.ts

--- server.ts
+++ server.ts
@@ -32,7 +32,7 @@ app.use(cookieParser());
 app.use(
   cors({
     //origin: "http://localhost:3001",
-    origin: "http://3.17.176.127",
+    origin: "http://3.15.1.210",
     credentials: true,
   })
 );
